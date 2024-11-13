d805da752d85bd24169c29ca9837759910b46ea2
Kibi <gponcet@protonmail.com>
2024-11-13 01:48:28 +0100

    Page login

--- remindr-web-flask/remindr-web-flask-app.py
+++ remindr-web-flask/remindr-web-flask-app.py
@@ -5,18 +5,24 @@ from flask import request
 app = Flask(__name__)
 
 def hello(name=None):
-    return render_template('remindr-web-flask.html', person=name)
+    return render_template('remindr-web-flask-app.html', person=name)
 
 @app.route("/")
 def hello_world():
     return hello("Greg")
-
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         return do_the_login()
     else:
         return show_the_login_form()
+
+def show_the_login_form():
+    return render_template('login.html')
+
+def do_the_login():
+    return "Logged"
 
 if __name__ == '__main__':
     app.run(debug=True)
