d7f6df8c089c23d3b35b9f0ac105e940ba75061b
Kibi <gponcet@protonmail.com>
2024-11-13 02:43:58 +0100

    Gestion login sessions

--- remindr-web-flask/remindr-web-flask-app.py
+++ remindr-web-flask/remindr-web-flask-app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
 
 app = Flask(__name__)
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 def hello(name=None):
     return render_template('remindr-web-flask-app.html', person=name)
@@ -10,9 +13,12 @@ def hello(name=None):
 @app.route("/")
 def hello_world():
     return hello("Greg")
+
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    app.logger.debug('Login in process...')
+
     if request.method == 'POST':
         return do_the_login()
     else:
@@ -22,7 +28,12 @@ def show_the_login_form():
     return render_template('login.html')
 
 def do_the_login():
-    return "Logged"
+    username = request.form['username']
+    if 'username' in session:
+        return render_template('login.html', username_in_session=session["username"])
+    else:
+        session['username'] = username
+        return show_the_login_form()
 
 if __name__ == '__main__':
     app.run(debug=True)
