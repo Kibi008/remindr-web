ba65446ae92490000e25083e0846f975ee49845d
Gpo <gponcet@protonmail.com>
2025-11-25 00:43:59 +0100

    login required

--- remindr-web-flask/auth.py
+++ remindr-web-flask/auth.py
@@ -9,6 +9,16 @@ from .db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     print(request.method)
@@ -73,16 +83,8 @@ def login():
     return render_template('auth/login.html')
 
 @bp.route('/logout')
+@login_required 
 def logout():
     session.clear()
     return redirect(url_for('index'))
 
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-
-        return view(**kwargs)
-
-    return wrapped_view
